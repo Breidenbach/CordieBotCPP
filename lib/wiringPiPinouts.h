2ca062ba9ab92685570e2533d442b9fbc3e839a4
Hal Breidenbach <hal@breidenbach.net>
2019-11-26 11:14:22 -0500

    Create wiringPiPinouts.h
    map from GPIO definitions to wiringPi pin numbers.

---
+++ lib/wiringPiPinouts.h
@@ -0,0 +1,37 @@
+/**
+ *   wiringPiPinouts
+ *
+ *   Map GPIO definitions to WiringPi definitions
+ *   From:  https://www.digikey.com/en/maker/blogs/2019/how-to-use-gpio-on-the-raspberry-pi-with-c
+ *
+ */
+ 
+#define GPIO0 0
+#define GPIO1 1
+#define GPIO2 2
+#define GPIO3 3
+#define GPIO4 4
+#define GPIO5 5
+#define GPIO6 6
+#define OneWire 7
+#define SDA1 8
+#define SCL1 9
+#define CE0 10
+#define CE1 11
+#define MOSI 12
+#define MISO 13
+#define SCLK 14
+#define TxD 15
+#define RxD 16
+#define GPIO21 21
+#define GPIO22 22
+#define GPIO23 23
+#define GPIO24 24
+#define GPIO25 25
+#define GPIO26 26
+#define GPIO27 27
+#define GPIO28 28
+#define GPIO29 29
+#define SDA0 30
+#define SCL0 31
+
