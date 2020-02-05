377363ed65a36830287298854daafb4bcbfa9096
Hal Breidenbach <hal@breidenbach.net>
2020-02-05 11:33:11 -0500

    Update cp_cordiebot.sh
    Add additiional files that may be copied if present:
    cp_cordiebot.sh
    cp_wpa_conf.sh
    keys.js

--- cp_cordiebot.sh
+++ cp_cordiebot.sh
@@ -11,4 +11,13 @@ if [ -f $1/cordiebot_listen ]; then
     cp $1/cordiebot_listen .
     chmod a+x cordiebot_listen
 fi
+if [ -f $1/cp_cordiebot.sh ]; then
+    cp $1/cp_cordiebot.sh .
+fi
+if [ -f $1/cp_wpa_conf.sh ]; then
+    cp $1/cp_wpa_conf.sh .
+fi
+if [ -f $1/keys.js ]; then
+    cp $1/keys.js .
+fi
 eject /media/pi/CORDIEBOT
